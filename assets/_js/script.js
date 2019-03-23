184e447af3a279e0bc8ccb6bffea97efa05e60b3
Diandra <leao.diandra@gmail.com>
2019-03-23 18:23:32 -0300

    estruturando form

--- assets/_js/script.js
+++ assets/_js/script.js
@@ -10,18 +10,10 @@ var DEFAULT = {
 			url: '//qr-challenge.herokuapp.com/api/v1/users',
 			headers: {
 				'Content-Type': 'application/json;charset=UTF-8'
-			},
-			data: { id: '',
-					name: '',
-					email: '',
-					photo_url: '',
-					admission_date: '',
-					job_title: '',
-					created_at: '',
-					updated_at: ''},
+			}
 		})
-		.done(function() {
-			console.log("success");
+		.done(function(resp) {
+			console.log(resp);
 		})
 		.fail(function() {
 			console.log("error");
